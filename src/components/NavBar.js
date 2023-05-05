4a694fd283ffc2759df81085521499bdfff7ec0d
idris <idreesnoor150@gmail.com>
2023-05-05 22:11:27 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,11 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a href="#home">home</a>
+  <a href="#about">about</a>
+  <a href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
